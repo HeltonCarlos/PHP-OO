6952f3871112755612b44cd372cce15ae80bab0a
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-25 17:56:42 -0300

    modulo03 README
    MODULO03

---
+++ modulo03/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>self-chamando constante</title>
+</head>
+<body>
+            <?php
+               <?php
+               class Pessoa{
+               const nome="Rodrigo";
+                      
+                      public function ColocarNome() {
+                          echo self::nome;
+                      }
+               }
+          
+              $pessoa= new Pessoa();
+              $pessoa->ColocarNome();
+          
+           ?>
+       
+            ?>
+</body>
+</html>
